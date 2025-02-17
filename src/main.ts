bcf1699a2e2fdfe8e5248ce3df62edf32311633f
elecbug <deveb1479@gmail.com>
2025-02-17 22:49:15 +0900

    ui: test 2

--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import './style.css';
-
-const app = document.getElementById('app') as HTMLDivElement;
-app.innerHTML = `<h1>Hello, World!</h1>`;
---
+++ src/scripts/main.ts
@@ -0,0 +1,7 @@
+import '../styles/content_style.css';
+import '../styles/not_found.css';
+import '../styles/style.css';
+import './load_md';
+import { initializeMarkdownLoader } from './load_md';
+
+initializeMarkdownLoader();
