26ad2c8f3cd4263232a865d73c38a7462cf7343a
DevNerd <krsy0411@naver.com>
2025-04-13 00:46:45 +0900

    [task/65] TableOfContents : IntersectionObserver 추가

--- src/scripts/table-contents.ts
+++ src/scripts/table-contents.ts
@@ -1,6 +1,22 @@
-// const renderTableContents = () => {};
+const createObserver = (headingMap: Record<string, HTMLLIElement>) => {
+  return new IntersectionObserver((entries) => {
+    const visibleEntry = entries
+      .filter((entry) => entry.isIntersecting)
+      .sort((a, b) => a.boundingClientRect.top - b.boundingClientRect.top)[0];
 
-// const highlightCurrentSection = () => {};
+    if (visibleEntry) {
+      const id = (visibleEntry.target as HTMLElement).id;
+
+      Object.entries(headingMap).forEach(([headingId, li]) => {
+        if (headingId === id) {
+          li.classList.add('border-l-2', 'border-blue-500');
+        } else {
+          li.classList.remove('border-l-2', 'border-blue-500');
+        }
+      });
+    }
+  });
+};
 
 export const initializeTableContents = () => {
   const content = document.getElementById('content') as HTMLElement;
@@ -13,10 +29,12 @@ export const initializeTableContents = () => {
   const tocList = document.createElement('ul');
   const headings = content.querySelectorAll('h2, h3');
 
-  tocTitle.classList.add('text-black', 'font-light', 'text-2xl', 'pb-3');
+  tocTitle.classList.add('text-black', 'font-light', 'text-2xl', 'pb-5');
   tocTitle.textContent = 'Table of contents';
 
-  headings.forEach((heading) => {
+  const headingMap: Record<string, HTMLLIElement> = {};
+
+  headings.forEach((heading, index) => {
     const listItem = document.createElement('li');
     listItem.classList.add(
       'max-w-64',
@@ -24,10 +42,10 @@ export const initializeTableContents = () => {
       'hover:bg-gray-300',
       'hover:font-light',
       'cursor-pointer',
-      'truncate',
-      'pb-1'
+      'truncate'
     );
     const link = document.createElement('a');
+    link.classList.add('flex', 'justify-start', 'items-stretch', 'p-1');
     link.textContent = heading.textContent || '';
 
     listItem.appendChild(link);
@@ -36,8 +54,14 @@ export const initializeTableContents = () => {
     if (heading.tagName === 'H3') {
       listItem.classList.add('pl-3');
     }
+
+    heading.id = `${index}`;
+    headingMap[heading.id] = listItem;
   });
 
   toc.appendChild(tocTitle);
   toc.appendChild(tocList);
+
+  const observer = createObserver(headingMap);
+  headings.forEach((heading) => observer.observe(heading));
 };
