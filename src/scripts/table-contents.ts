085074c278d9433b709ea3bc9c43fb47643b966e
DevNerd <krsy0411@naver.com>
2025-04-12 22:51:44 +0900

    [task/65] TableOfContents : CSS 설정 초기 작업 완료

--- src/scripts/table-contents.ts
+++ src/scripts/table-contents.ts
@@ -1,37 +1,43 @@
+// const renderTableContents = () => {};
+
+// const highlightCurrentSection = () => {};
+
 export const initializeTableContents = () => {
   const content = document.getElementById('content') as HTMLElement;
-  const tableOfContents = document.getElementById(
-    'TableOfContents'
-  ) as HTMLElement;
+  const toc = document.getElementById('toc') as HTMLElement;
 
-  tableOfContents.innerHTML = '';
+  toc.innerHTML = '';
   if (!content.querySelector('h1')) return;
 
+  const tocTitle = document.createElement('p');
+  const tocList = document.createElement('ul');
   const headings = content.querySelectorAll('h2, h3');
-  const tableContentsList = document.createElement('ul');
+
+  tocTitle.classList.add('text-black', 'font-light', 'text-2xl', 'pb-3');
+  tocTitle.textContent = 'Table of contents';
 
   headings.forEach((heading) => {
     const listItem = document.createElement('li');
     listItem.classList.add(
       'max-w-64',
+      'font-extralight',
       'hover:bg-gray-300',
-      'hover:font-semibold',
+      'hover:font-light',
       'cursor-pointer',
       'truncate',
-      'p-1'
+      'pb-1'
     );
     const link = document.createElement('a');
-    // 👇👇👇 문서 목차에 스크린 이동 👇👇👇
-    // link.href = `#${heading.id}`;
     link.textContent = heading.textContent || '';
 
     listItem.appendChild(link);
-    tableContentsList.appendChild(listItem);
+    tocList.appendChild(listItem);
 
     if (heading.tagName === 'H3') {
       listItem.classList.add('pl-3');
     }
   });
 
-  tableOfContents.appendChild(tableContentsList);
+  toc.appendChild(tocTitle);
+  toc.appendChild(tocList);
 };
