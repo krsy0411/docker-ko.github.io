f23f119958a4002880aca0bc5d529534355d8382
DevNerd <krsy0411@naver.com>
2025-05-08 23:06:16 +0900

    [환경설정] eslint+prettier 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
   },
