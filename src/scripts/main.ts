871fc2b92f0bc429aba515afb2465f21b0d4e63f
ThisTimeNull <krsy0411@naver.com>
2025-06-28 15:33:39 +0900

    [npm] applicationinsights : SDK에서 패키지로 전환

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -6,6 +6,18 @@ import './components/card-component';
 import { initializeMarkdownLoader } from './load_md';
 import { initializeTableContents } from './table-contents';
 import { initializeBreadcrumb } from './breadcrumb';
+import appInsights from 'applicationinsights';
+appInsights.setup("InstrumentationKey=7bea0293-01dc-409c-9471-3a65567b11ed;IngestionEndpoint=https://koreacentral-0.in.applicationinsights.azure.com/;LiveEndpoint=https://koreacentral.livediagnostics.monitor.azure.com/;ApplicationId=ddbe985c-4d7a-41e4-80a8-a3961068933b")
+    .setAutoCollectRequests(true)
+    .setAutoCollectPerformance(true, true)
+    .setAutoCollectExceptions(true)
+    .setAutoCollectDependencies(true)
+    .setAutoCollectConsole(true, false)
+    .setAutoCollectPreAggregatedMetrics(true)
+    .setSendLiveMetrics(false)
+    .setInternalLogging(false, true)
+    .enableWebInstrumentation(false)
+    .start();
 
 document.addEventListener('DOMContentLoaded', async () => {
   try {
